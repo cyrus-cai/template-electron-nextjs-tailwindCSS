d865ab113263f3e041a44962386c530c41cf4059
xiikii <andy864940012@gmail.com>
2023-06-19 23:09:53 +0800

    fix:commands

--- cli.js
+++ cli.js
@@ -2,40 +2,43 @@
 
 const fs = require("fs-extra");
 const path = require("path");
-const readline = require("readline");
 
 // 获取命令行参数
 const args = process.argv.slice(2);
 
-// 获取新项目的名称
-let projectName = args[0];
+let projectName = '';
 
-// 创建 readline.Interface 实例
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// 确保项目名称已提供
-if (!projectName) {
-  rl.question("Please provide a project name: ", (answer) => {
-    projectName = answer;
-    createProject(projectName);
-    rl.close();
-  });
-} else {
-  createProject(projectName);
-  rl.close();
+// Check for '--' pattern and extract project name
+for (let i = 0; i < args.length; i++) {
+  if (args[i].startsWith('--')) {
+    // If project name is immediately following '--'
+    if (args[i].length > 2) {
+      projectName = args[i].slice(2);
+    }
+    // If project name is the next argument
+    else if (i < args.length - 1) {
+      projectName = args[i + 1];
+    }
+    break;
+  }
 }
 
+if (!projectName) {
+  console.error("Command must be in the format: 'command --projectname' or 'command -- projectname'");
+  process.exit(1);
+}
+
+const templateDir = path.join(__dirname, 'template');
+
+createProject(projectName);
+
 function createProject(projectName) {
-  fs.copySync(__dirname, path.join(process.cwd(), projectName), {
+  fs.copySync(templateDir, path.join(process.cwd(), projectName), {
     filter: (src) => {
-      // 忽略 cli.js 文件
-      return path.basename(src) !== 'cli.js';
+      // 忽略 node_modules 文件夹
+      return !src.includes('node_modules');
     }
   });
-
 
   console.log(`Project created: ${projectName}`);
 }
